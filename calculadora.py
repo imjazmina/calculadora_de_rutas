c30137869db233ca40f5b04cb7042c5d49b95f38
maria <alonsomoralesjazmin@gmail.com>
2025-06-24 12:07:43 -0300

    Mostrar mapa con entrada y salida

--- calculadora.py
+++ calculadora.py
@@ -3,18 +3,16 @@ filas = int(input("Ingrese el numero de filas: "))
 columnas = int(input("ingrese el numero de columnas: "))
 mapa = [[" " for _ in range(filas)] for _ in range(columnas)]
 
-#salida =  int(input("Ingrese la coordenada de salida: "))
+#ingresar las coordenadas entreada y salida
+fil_entrada, col_entrada = map(int, input("Ingrese la coordenada de entrada separado por comas: ").split(","))
+fil_salida, col_salida = map(int, input("Ingrese las coordenadas de la salida separado por comas: ").split(","))
 
-fil_entrada, col_entrada = int(input("Ingrese la coordenada de entrada: ")).split(",")
-fil_salida, col_salida = salida.split()
-
-mapa[fil_entrada][col_entrada] == 'E'
-mapa[fil_salida][col_salida] == 'S'
+mapa[fil_entrada][col_entrada] = 'E'
+mapa[fil_salida][col_salida] = 'S'
 
 for row in mapa:
     for celda in row:
         print(f'[{celda}]', end= ' ')
     print("\n")
 
-#Ingresar coordenadas de entrada y salida
-
+#agregar obstaculos
