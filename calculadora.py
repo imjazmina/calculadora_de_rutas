cd0c83bb83989e482389400871f8c780f0273ca0
maria <alonsomoralesjazmin@gmail.com>
2025-06-27 00:34:42 -0300

    insertar obstaculos por consola

--- calculadora.py
+++ calculadora.py
@@ -55,16 +55,42 @@ def generar_obstaculos(filas, columnas, cantidad_obstaculos, mapa, fil_entrada, 
         if (
             (nueva_x, nueva_y) != (fil_entrada, col_entrada)
             and (nueva_x, nueva_y) != (fil_salida, col_salida)
-            and 0 < nueva_x < columnas
-            and 0 < nueva_y < filas
+            and coordenadas_validas(nueva_x,nueva_y)
             and mapa[nueva_x][nueva_y] == 0
             #validar que los adyacentes no sean 3
         ):
             mapa[nueva_x][nueva_y] = random.choice(obstaculos)
             colocados += 1
 
+def insertar_obstaculo(mapa, fila, columna):
+    while True: 
+        x_obstaculo, y_obstaculo = map(int,input("ingrese las coordenadas del nuevo obstaculo separado por comas").split(","))
+        if not coordenadas_validas(x_obstaculo, y_obstaculo):
+            print("Coordenadas invalidas. Ingrese nuevamente")
+        elif (x_obstaculo, y_obstaculo) == (fil_entrada, col_entrada) and (x_obstaculo, y_obstaculo) == (fil_salida, col_salida):
+            print("El obstaculo no puede estar sobre la entrada o salida")
+        else:
+            break
+            
+    tipo_obstaculo = int(input("Ingrese el tipo de obstaculo. 1 para 🏛️. 2 para 🌊. 3 para 🚧. : "))
+    if tipo_obstaculo in [1, 2, 3]:
+        mapa[x_obstaculo][y_obstaculo] = tipo_obstaculo
+    else:
+        print("Ingrese una opcion valida")
+    
+    return mapa[x_obstaculo][y_obstaculo]
+
 cantidad_obstaculos = filas * columnas / 3
 generar_obstaculos(filas, columnas, cantidad_obstaculos, mapa, fil_entrada, col_entrada, fil_salida, col_salida)
 mapa[fil_entrada][col_entrada] = 'E'
 mapa[fil_salida][col_salida] = 'S'
 imprimir_mapa(mapa)
+
+#funcion agregar obstaculo nuevo
+opcion = input("Desea agregar/editar algun obstaculo? Y/N \nPresione N para salir: ").upper()
+while opcion != 'N':
+    insertar_obstaculo(mapa, filas, columnas)
+    imprimir_mapa(mapa)
+    opcion = input("Desea agregar/editar otro obstaculo? Y/N \nPresione N para salir: ").upper()
+
+
