2d92a09744da52897242639c6370ad22cf1638ed
maria <alonsomoralesjazmin@gmail.com>
2025-06-30 14:32:07 -0300

    algoritmo a estrella

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,5 @@
 import random
+from heapq import heappush, heappop
 
 #Matriz bidimensional
 filas = int(input("Ingrese el numero de filas: "))
@@ -27,13 +28,15 @@ def imprimir_mapa(mapa):
     for row in mapa:
         for celda in row:
             if celda == 0:
-                print(f'[0]', end= ' ')
+                print('[ ]', end= ' ')
             elif celda == 1:
-                print(f'[🏛️]', end= ' ')
+                print('[🏛️]', end= ' ') 
             elif celda == 2:
-                print(f'[🌊]', end= ' ')
+                print('[🌊]', end= ' ')
             elif celda == 3:
-                print(f'[🚧]', end= ' ')
+                print('[🚧]', end= ' ')
+            elif celda == '*':
+                print('[✅]', end=' ')
             else:
                 print(f'[{celda}]', end=' ')  
         print("\n")
@@ -62,7 +65,7 @@ def generar_obstaculos(filas, columnas, cantidad_obstaculos, mapa, fil_entrada, 
             mapa[nueva_x][nueva_y] = random.choice(obstaculos)
             colocados += 1
 
-def insertar_obstaculo(mapa, fila, columna):
+def insertar_obstaculo(mapa, fila, columna):# fila y columna no se utilizan
     while True: 
         x_obstaculo, y_obstaculo = map(int,input("ingrese las coordenadas del nuevo obstaculo separado por comas").split(","))
         if not coordenadas_validas(x_obstaculo, y_obstaculo):
@@ -80,17 +83,100 @@ def insertar_obstaculo(mapa, fila, columna):
     
     return mapa[x_obstaculo][y_obstaculo]
 
-cantidad_obstaculos = filas * columnas / 3
+# A ⭐
+#heuristica
+def distancia_manhatan(fil_entrada, col_entrada, fil_salida, col_salida):
+    return abs(fil_entrada-fil_salida) + abs(col_entrada - col_salida)
+
+# funcion para crear la lista con las coordenadas del camino mas corto una vez llegado al objetivo
+def reconstruir_camino(visitado, pos_actual):
+    camino = [pos_actual]
+    while pos_actual in visitado: 
+        pos_actual = visitado[pos_actual] 
+        camino.append(pos_actual) 
+    camino.reverse()#reversa los elementos en la lista para que al imprimir lo muestre desde el inicio 
+    return camino
+
+def obtener_movimientos(pos, filas, columnas):
+    movimientos = []
+    x, y = pos
+    direcciones = [(-1, 0), (1, 0), (0, -1), (0, 1)]  
+    for dx, dy in direcciones:
+        nueva_x = x + dx
+        nueva_y = y + dy
+        if 0 <= nueva_x < filas and 0 <= nueva_y < columnas:
+            movimientos.append((nueva_x, nueva_y))
+    return movimientos
+
+def a_estrella(mapa, filas, columnas, fil_entrada, fil_salida, col_entrada, col_salida):
+    por_recorrer = []
+    pos_inicio = (fil_entrada, col_entrada)
+    pos_salida = (fil_salida, col_salida)
+
+    heappush(por_recorrer, (0, pos_inicio))
+
+    visitado = {}
+    g_score = {pos_inicio: 0}
+    f_score = {pos_inicio: distancia_manhatan(*pos_inicio, *pos_salida)}
+
+    while por_recorrer:
+        _, actual = heappop(por_recorrer)
+
+        if actual == pos_salida:
+            return reconstruir_camino(visitado, actual)
+        
+        movimientos = obtener_movimientos(actual, filas, columnas)
+        for movimiento in movimientos:
+
+            if mapa[movimiento[0]][movimiento[1]] in [1,2,3]:
+                continue
+
+            intento_g_score = g_score[actual] + 1
+            if movimiento not in g_score or intento_g_score < g_score[movimiento]:
+                visitado[movimiento] = actual
+                g_score[movimiento] = intento_g_score
+                f_score[movimiento] = intento_g_score + distancia_manhatan(*movimiento, *pos_salida)
+                heappush(por_recorrer, (f_score[movimiento], movimiento))
+
+    return None # no se encontro un camino
+  
+
+cantidad_obstaculos = int(filas * columnas / 3)
 generar_obstaculos(filas, columnas, cantidad_obstaculos, mapa, fil_entrada, col_entrada, fil_salida, col_salida)
 mapa[fil_entrada][col_entrada] = 'E'
 mapa[fil_salida][col_salida] = 'S'
+camino = a_estrella(mapa, filas, columnas, fil_entrada, fil_salida, col_entrada, col_salida)
+if camino:
+    for x, y in camino:
+        if (x, y) != (fil_entrada, col_entrada) and (x, y) != (fil_salida, col_salida):
+            mapa[x][y] = '*'
+else:
+    print("No se encontro un camino al destino.")
+
 imprimir_mapa(mapa)
+#funcion limpiar mapa
+for i in range(filas):
+    for j in range(columnas):
+        if mapa[i][j] == '*':
+            mapa[i][j] = 0
+
 
 #funcion agregar obstaculo nuevo
 opcion = input("Desea agregar/editar algun obstaculo? Y/N \nPresione N para salir: ").upper()
 while opcion != 'N':
     insertar_obstaculo(mapa, filas, columnas)
+    #funcion limpiar mapa
+    for i in range(filas):
+        for j in range(columnas):
+            if mapa[i][j] == '*':
+                mapa[i][j] = 0
+    camino = a_estrella(mapa, filas, columnas, fil_entrada, fil_salida, col_entrada, col_salida)
+    
+    if camino:
+        for x, y in camino:
+            if (x, y) != (fil_entrada, col_entrada) and (x, y) != (fil_salida, col_salida):
+                mapa[x][y] = '*'
     imprimir_mapa(mapa)
-    opcion = input("Desea agregar/editar otro obstaculo? Y/N \nPresione N para salir: ").upper()
 
+    opcion = input("Desea agregar/editar otro obstaculo? Y/N \nPresione N para salir: ").upper()
 
